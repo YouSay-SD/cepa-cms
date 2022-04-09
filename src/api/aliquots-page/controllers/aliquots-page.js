658a4d68b06f9d953fe9f2caf8bbe2ac8eaddd87
YouSay <yousay.sd@gmail.com>
2022-04-09 20:28:48 -0300

    create aliquots page scheme

---
+++ src/api/aliquots-page/controllers/aliquots-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  aliquots-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::aliquots-page.aliquots-page');
