d5838780f34d6f5bed709e52527971020225de26
YouSay <yousay.sd@gmail.com>
2022-04-13 02:46:03 -0300

    feat: create country category

---
+++ src/api/country-categories-tax-haven/controllers/country-categories-tax-haven.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  country-categories-tax-haven controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::country-categories-tax-haven.country-categories-tax-haven');
---
+++ src/api/country-categories-tax-haven/routes/country-categories-tax-haven.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * country-categories-tax-haven router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::country-categories-tax-haven.country-categories-tax-haven');
